f2fbc476a3027a18418a7cb1eb6b18e2b8e97904
Andrii Yozh <39120801+Yojuks@users.noreply.github.com>
2021-10-21 17:35:23 +0300

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://18.170.221.142/api',
 });
 
 export const insertTodoItem = (payload) => api.post(`/todolist`, payload);
